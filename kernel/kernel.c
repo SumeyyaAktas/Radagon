b9867c7006a51f44f6560e3bcdd23d1cf22f7178
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-22 18:55:10 -0500

    move serial funcs out

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,47 +1,14 @@
 #include <stdint.h>
 #include "include/ports.h"
 #include <stdbool.h>
-#include "include/vga.h"
+#include "vga.h"
+#include "serial.h"
 
-#define COM1 0x3F8
-
-static bool serial_initialized = false;
-
-static inline int is_transmit_empty(void) 
+void hcf(void)
 {
-    return inb(COM1 + 5) & 0x20;
-}
-
-bool serial_init(void) 
-{
-    outb(COM1 + 1, 0x00);
-    outb(COM1 + 3, 0x80);
-    outb(COM1 + 0, 0x03);
-    outb(COM1 + 1, 0x00);
-    outb(COM1 + 3, 0x03);
-    outb(COM1 + 2, 0xC7);
-    outb(COM1 + 4, 0x0B);
-
-    serial_initialized = true;
-    return true;
-}
-
-void serial_write_char(char c) 
-{
-    if (!serial_initialized) 
+    for (;;)
     {
-        return;
-    }
-    
-    while (!is_transmit_empty());
-    outb(COM1, c);
-}
-
-void serial_print(const char* str) 
-{
-    while (*str) 
-    {
-        serial_write_char(*str++);
+        asm("hlt");
     }
 }
 
@@ -49,11 +16,9 @@ void kernel_main(void)
 {
     vga_init();
     serial_init();
+
     serial_print("\n64-bit kernel running!\n");
     vga_print("\n\n\n\n\n\n64-bit kernel running!\n");
     
-    while (1) 
-    {
-        __asm__ __volatile__("hlt");
-    }
+    hcf();
 }
